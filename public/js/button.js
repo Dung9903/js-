6103292829cb52a17f68635fbc2b708240abbfdb
DUNG <dungnguyen9903@gmail.com>
2022-12-20 08:28:36 +0700

    js

---
+++ public/js/button.js
@@ -0,0 +1,8 @@
+$(document).on('click', '#success',function(e) {
+    swal(
+        'Thành công',
+        'Bạn thanh toán <b style="color:green;">thành công</b>!',
+        'success'
+    )
+});
+
